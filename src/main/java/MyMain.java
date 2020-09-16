6334e4297a33adb46ea6bb03ae26e6a75a046384
Kiran-O <22kokelly@athenian.org>
2020-09-16 00:22:45 +0000

    Passed all tests

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -3,12 +3,31 @@ import java.util.Scanner;
 public class MyMain {
     
     public static int binaryToDecimal(String binary) { 
-        return -1;
-        // REPLACE WITH YOUR CODE HERE
+        int zero = 0;
+        int one = 1;
+
+        int wer = binary.length();
+        
+        for (int k = wer -1; k >= 0; k--){
+
+            if (binary.charAt(k)=='1'){
+
+                zero += one;
+            }
+            one = one*2;
+        }
+        return zero;
+
+
     }
     
     
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Type in a number in binary format:");
+        String binary = scan.next();
+        System.out.println("That is equal to the decimal value: "+ binaryToDecimal(binary));
+
+
     }
 }
